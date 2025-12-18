88ed26cda6a09fc983c98be19080de29cea42740
Ritushna-a <ritushnaamatya@gmail.com>
2025-12-18 20:38:55 +0545

    Initial commit

---
+++ app/src/main/java/com/example/learnnex/ui/theme/Color.kt
@@ -0,0 +1,16 @@
+package com.example.learnnex.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFFFFFFF)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF5C6BC0)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+val Purple = Color(0xFFB39DDB)
+val Blue = Color(0xFF5C6BC0)
+
+val White = Color(0xFFFBFBFD)
+val DarkBlue= Color(0xFF2222A6)
